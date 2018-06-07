427e5cd5f28dc266e9114a9b04c97674fb761de1
xuxinlong <18170551225@163.com>
2018-06-07 21:20:08 +0800

    添加文章详情页

---
+++ article/detail.js
@@ -0,0 +1,95 @@
+(function () {
+    var Header = xl_components.Header;
+    var List = xl_components.List;
+
+    var Articleinfo = React.createClass({
+        getInitialState: function () {
+            return {
+            };
+        },
+        componentDidMount: function () {
+        },
+        render: function() {
+            return (
+                <div className="article-header">
+                    <div className="article-title">{this.props.detail ? this.props.detail.title : ''}</div>
+                    <div className="article-info">
+                        <div className="info">
+                          <span>作者：{this.props.user_info ? this.props.user_info.name : ''}</span>
+                          <div><span>日期：{this.props.detail ? this.props.detail.time : ''}</span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span> 字数:{(this.props.detail && this.props.detail.text) ? this.props.detail.text.length : ''}</span></div>
+                        </div>
+                        {(this.props.detail && this.props.detail.isAuther) ? <div className="edit-btn">编辑</div> : ''}
+                    </div>
+                </div>
+            );
+        }
+    });
+    var Articletext = React.createClass({
+        getInitialState: function () {
+            return {};
+        },
+        componentWillReceiveProps(nextProps) {
+            document.getElementById('markdown_body').innerHTML = nextProps.text;
+        },
+        componentDidMount: function () {
+            document.getElementById('markdown_body').innerHTML = this.props.text;
+        },
+        render: function() {
+            return (
+                <div className="markdown-body" id="markdown_body">
+                </div>
+            );
+        }
+    });
+
+    var Container = React.createClass({
+        getInitialState: function () {
+            return {
+                'user_info': {},
+                'detail': {},
+                'text': ''
+            };
+        },
+        componentDidMount: function () {
+            var url = '/blog/article/detail';
+            var params = xl.parseHash();
+            var param = {'id': params.id};
+            xl_fetch(url, {
+                'method': 'post',
+                'body': JSON.stringify(param),
+                'headers': {
+                    'content-type': 'application/json'
+                }
+            }).then((res) => {
+                if (res.code === 0) {
+                    var parser = new Mditor.Parser();
+                    this.setState({
+                        'user_info': res.data.user_info,
+                        'detail': res.data.detail,
+                        'text': parser.parse(res.data.detail.text)
+                    });
+                    window.text = res.data.detail.text;
+                }
+            });
+        },
+        render: function() {
+            return (
+                <div className="container">
+                    <Articleinfo user_info={this.state.user_info} detail={this.state.detail}></Articleinfo>
+                    <Articletext text={this.state.text}></Articletext>
+                </div>
+            );
+        }
+    });
+
+    ReactDOM.render(
+        (
+        <div>
+            <Header></Header>
+            <Container></Container>
+        </div>
+        )
+        ,
+        document.getElementById('main')
+    );
+})();
